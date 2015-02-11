09b5876972181711334a628bd992398de4f2d972
jiaweih <jiaweihe@Jiaweis-MacBook-Pro.local>
2015-02-10 20:02:52 -0800

    add functions of calculating dates of maximum annual events

--- vicData.py
+++ vicData.py
@@ -15,7 +15,7 @@ output_idr: output directories to store processed output files
 
 Returns
 ---------
-Output files contain four columns: year, maximum prec, maximum run_base, annual mean prec
+Output files contain four columns: year, annuam max prec, annual max run_base, annual mean prec
 
 Examples
 ---------
@@ -23,7 +23,6 @@ python vicData.py -i example_fluxes -o example_outputs
 Note: if vicData.py is not under the same directory as input_dir and output_dir, absolute 
 	  path of input_dir and output_dir will be required
 """
-
 
 import numpy as np
 import pandas as pd
@@ -33,12 +32,15 @@ import argparse
 
 
 def main():
-    in_dir,out_dir = command_lines()
+    in_dir,out_dir_data,out_dir_dates_prec,out_dir_dates_runbase = command_lines()
     for filename in os.listdir(in_dir):
     	in_path = os.path.join(in_dir,filename)
         df = read_files(in_path)
-        #df_max,df_mean = get_max_mean(df)
-        write_files(df,out_dir,filename)
+        write_data = calculate_data(df)
+        write_files_data(write_data,out_dir_data,filename)
+        dates_prec_max,dates_run_base_max = calculate_dates(df,write_data)
+        write_files_dates(dates_prec_max,out_dir_dates_prec,filename)
+        write_files_dates(dates_run_base_max,out_dir_dates_runbase,filename)
 
 def read_files(in_path,startYear=1915,endYear=2006,startIndex=274,endIndex=33511):
     """
@@ -59,14 +61,6 @@ def read_files(in_path,startYear=1915,endYear=2006,startIndex=274,endIndex=33511
     df = pd.DataFrame(data_extract, index=dates, columns = header)
     return df
 
-def get_max_mean(df):
-    """
-    Read in daily DataFrames and produce annual maximum and mean DataFrames
-    """
-    df_max = df.resample('A',how='max')
-    df_mean = df.resample('A',how='mean')
-    return df_max,df_mean
-
 def water_year(in_date):
     if in_date.year%4 == 0:
         if in_date.timetuple().tm_yday <= 274:
@@ -79,20 +73,45 @@ def water_year(in_date):
         else:
             return in_date.year + 1 
 
-def write_files(df,out_dir,filename,startYear=1916,endYear=2006):
-    """
-    Read in annual maximum DataFrames and save values in ascill files
-
-    """
+def calculate_data(df,startYear=1916,endYear=2006):
     years = np.arange(startYear,endYear+1)
     prec_max = df['prec'].groupby(lambda x:water_year(x)).max().values
     prec_mean = df['prec'].groupby(lambda x:water_year(x)).mean().values
     run_base_max = df['run_base'].groupby(lambda x:water_year(x)).max().values
-    write_arrays = np.transpose([years,prec_max,run_base_max,prec_mean])
+    write_data = np.transpose([years,prec_max,run_base_max,prec_mean])
+    return write_data
+
+def calculate_dates(df,write_data):
+    prec_max = write_data[:,1]
+    run_base_max = write_data[:,2]
+    dates_prec_max = get_dates(df,prec_max,'prec')
+    dates_run_base_max = get_dates(df,run_base_max,'run_base')
+    return dates_prec_max,dates_run_base_max
+
+def get_dates(df,data,var,startYear=1916):
+    dates_max = np.empty([len(data),4])
+    for i in np.arange(len(data)):
+        index = df[df[var] == data[i]].index  ##There are more than two dates matching maximum annual values in some years 
+        for j in np.arange(len(index)):
+            if water_year(datetime(index[j].year,index[j].month,index[j].day)) == i + startYear:
+                dates_max[i,:] = np.array([index[j].year,index[j].month,index[j].day,data[i]])
+    return dates_max
+
+def write_files_data(write_data,out_dir,filename):
+    """
+    Read in annual maximum DataFrames and save values in ascill files
+
+    """
     if not os.path.exists(out_dir):
     	os.makedirs(out_dir)
     out_path = os.path.join(out_dir,filename)
-    np.savetxt(out_path,write_arrays,fmt=('%i','%8.3f','%8.3f','%8.3f'))
+    np.savetxt(out_path,write_data,fmt=('%i','%8.3f','%8.3f','%8.3f'))
+
+def write_files_dates(write_dates,out_dir,filename):
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    out_path = os.path.join(out_dir,filename)
+    np.savetxt(out_path,write_dates,fmt=('%4i','%4i','%4i','%8.3f'))
 
 def command_lines():
     parser = argparse.ArgumentParser()
@@ -100,12 +119,20 @@ def command_lines():
                         type=str,
                         help="Input directory",
                         required=True)
-    parser.add_argument("-o", "--output_dir",
+    parser.add_argument("-o", "--output_dir_data",
                         type=str,
-                        help="Output directory",
+                        help="Output data directory",
+                        required=True)
+    parser.add_argument("-prec", "--output_dir_dates_prec",
+                        type=str,
+                        help="Output dates directory for maximum annual precipitation",
+                        required=True)
+    parser.add_argument("-run_base", "--output_dir_dates_run_base",
+                        type=str,
+                        help="Output dates directory for maximum annual run_base",
                         required=True)
     args = parser.parse_args()
-    return args.input_dir,args.output_dir
+    return args.input_dir,args.output_dir_data,args.output_dir_dates_prec,args.output_dir_dates_run_base
 
 if __name__ == "__main__":
 	main()
